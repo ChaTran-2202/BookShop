76321acdad6c0ecf7a09c3724ed6493b9998fc10
ChaTran <160729001+chatran2202@users.noreply.github.com>
2024-09-09 22:09:47 +0700

    Remove secret from code

---
+++ BookShopWeb/Program.cs
@@ -0,0 +1,84 @@
+using BookShop.DataAccess.Repository;
+using BookShop.DataAccess.Repository.IRepository;
+using BookShop.DataAcess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BookShop.Utility;
+using Stripe;
+using BookShop.DataAccess.DbInitializer;
+using BookShop.DataAccess.DbInitializer;
+using BookShop.DataAccess.Repository.IRepository;
+using BookShop.DataAccess.Repository;
+using BookShop.DataAcess.Data;
+using BookShop.Utility;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options => {
+	options.LoginPath = $"/Identity/Account/Login";
+	options.LogoutPath = $"/Identity/Account/Logout";
+	options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+builder.Services.AddAuthentication().AddFacebook(option => {
+	option.AppId = builder.Configuration["Authentication:Facebook:AppId"];
+	option.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
+});
+builder.Services.AddAuthentication().AddMicrosoftAccount(option => {
+	option.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"];
+	option.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
+});
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options => {
+	options.IdleTimeout = TimeSpan.FromMinutes(100);
+	options.Cookie.HttpOnly = true;
+	options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+SeedDatabase();
+app.MapRazorPages();
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+void SeedDatabase()
+{
+	using (var scope = app.Services.CreateScope())
+	{
+		var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+		dbInitializer.Initialize();
+	}
+}
